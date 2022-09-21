9cf15927b01cb75dc2fdc81b6ad8e920fad1ed7c
EdgeInf <111714642+EdgeInf@users.noreply.github.com>
2022-09-21 03:37:54 -0400

    Update pants.lua

--- pants.lua
+++ pants.lua
@@ -1,1 +1,1 @@
-game:GetService("Workspace").Game.Players.luluuluvrr.Pants.PantsTemplate = = "rbxassetid://6846657836"
+game:GetService("Workspace").Game.Players.luluuluvrr.Pants.PantsTemplate = "rbxassetid://6846657836"
