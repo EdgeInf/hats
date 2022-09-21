adf51ce02b83b7211f5cbc20f5a5c8ce7b0716b4
EdgeInf <111714642+EdgeInf@users.noreply.github.com>
2022-09-21 02:54:03 -0400

    Update diamondevade.lua

--- diamondevade.lua
+++ diamondevade.lua
@@ -1,1 +1,1 @@
-game:GetService("Workspace").Game.Players.luluuluvrr.Head.Mesh.MeshId = "http://roblox.com/asset/?id=5591363207"
+game:GetService("Workspace").Game.Players.LocalPlayer.Head.Mesh.MeshId = "http://roblox.com/asset/?id=5591363207"
